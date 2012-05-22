d00fa44ca2101a9b6da6d699c032c8bbecfa30f0
eme <eme@545492.no-reply.drupal.org>
2012-05-22 19:08:47 +0200

    First commit.

---
+++ ajax_form_entity.js
@@ -0,0 +1,15 @@
+(function ($) {
+  Drupal.behaviors.entityboxes = { 
+    attach: function(context, settings) {
+      
+      // Trigger the upload button automatically when a file of image is chosen      
+      $('.field-type-image input.form-file:not(.auto_upload-processed)', context).addClass('auto_upload-processed').change(function() {
+        $(this).parent().find('input.form-submit').mousedown();
+      });
+      $('.field-type-file input.form-file:not(.auto_upload-processed)', context).addClass('auto_upload-processed').change(function() {
+        $(this).parent().find('input.form-submit').mousedown();
+      });
+
+    }
+  };
+})(jQuery);
