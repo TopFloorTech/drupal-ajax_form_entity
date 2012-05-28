e99dea8338c880befe8be28d7f28ef6ff8e8e5d6
eme <eme@545492.no-reply.drupal.org>
2012-05-28 14:38:17 +0200

    Code cleanup. Ok with coder review (still some drupal code sniffer elements).

--- ajax_form_entity.js
+++ ajax_form_entity.js
@@ -1,7 +1,6 @@
 (function ($) {
-  Drupal.behaviors.entityboxes = { 
+  Drupal.behaviors.entityboxes = {
     attach: function(context, settings) {
-      
       // Trigger the upload button automatically when a file of image is chosen      
       $('.field-type-image input.form-file:not(.auto_upload-processed)', context).addClass('auto_upload-processed').change(function() {
         $(this).parent().find('input.form-submit').mousedown();
@@ -9,7 +8,6 @@
       $('.field-type-file input.form-file:not(.auto_upload-processed)', context).addClass('auto_upload-processed').change(function() {
         $(this).parent().find('input.form-submit').mousedown();
       });
-
     }
   };
 })(jQuery);
