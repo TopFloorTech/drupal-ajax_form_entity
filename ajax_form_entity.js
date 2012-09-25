7be6a6b3db7f5211042312c5b7d12c09bbaff65e
MichaelB <ml.bornand@gmail.com>
2012-09-25 11:50:46 +0200

    Field collection revision and Code cleanup

--- ajax_form_entity.js
+++ ajax_form_entity.js
@@ -8,6 +8,34 @@
       $('.field-type-file input.form-file:not(.auto_upload-processed)', context).addClass('auto_upload-processed').change(function() {
         $(this).parent().find('input.form-submit').mousedown();
       });
+      //$('.form-collection').hide();
+      var LinkCollection = $('.open-form');
+      LinkCollection.unbind();
+      LinkCollection.click(function(event)
+      {
+       // LinkCollection.unbind("click");
+        var nameLink = $(this).attr('class');
+        var reg= new RegExp('[ ]+','g');
+        var tableau=nameLink.split(reg);
+        var e = document.getElementById(tableau[1]);
+        var link = document.getElementById('open-form'+tableau[1])
+        e.style.display = 'block';
+        link.style.display = 'none';
+        $('a.cancel-'+tableau[1]).show();
+        /*if(e.style.display == 'block')
+          {
+          e.style.display = 'none';
+          link.className = "open-form "+tableau[1];
+          link.innerHTML = Drupal.t("Add an item");
+          }
+        else
+          {
+          $('a.cancel-'+tableau[1]).show();
+          e.style.display = 'block';
+          link.className = "button-cancel "+tableau[1];
+          link.innerHTML = Drupal.t("Fermer");
+          }*/
+      })
     }
   };
 })(jQuery);
