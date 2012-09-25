6c7f1f875cd0e1036249ca129b6e847f30660d5f
MichaelB <ml.bornand@gmail.com>
2012-09-25 14:15:23 +0200

    fix bugs

--- ajax_form_entity.js
+++ ajax_form_entity.js
@@ -8,12 +8,12 @@
       $('.field-type-file input.form-file:not(.auto_upload-processed)', context).addClass('auto_upload-processed').change(function() {
         $(this).parent().find('input.form-submit').mousedown();
       });
-      //$('.form-collection').hide();
+      
+      // Display field collections forms.
       var LinkCollection = $('.open-form');
       LinkCollection.unbind();
       LinkCollection.click(function(event)
       {
-       // LinkCollection.unbind("click");
         var nameLink = $(this).attr('class');
         var reg= new RegExp('[ ]+','g');
         var tableau=nameLink.split(reg);
@@ -22,19 +22,6 @@
         e.style.display = 'block';
         link.style.display = 'none';
         $('a.cancel-'+tableau[1]).show();
-        /*if(e.style.display == 'block')
-          {
-          e.style.display = 'none';
-          link.className = "open-form "+tableau[1];
-          link.innerHTML = Drupal.t("Add an item");
-          }
-        else
-          {
-          $('a.cancel-'+tableau[1]).show();
-          e.style.display = 'block';
-          link.className = "button-cancel "+tableau[1];
-          link.innerHTML = Drupal.t("Fermer");
-          }*/
       })
     }
   };
